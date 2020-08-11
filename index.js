983b0b31ccb27292a31867ccc58e307a4fab7ea1
Rayed <rayed.benbrahim@gmail.com>
2020-08-11 16:38:30 +0200

    feat(index):get parking/id route

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
 
+app.get('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id === id)
+    res.status(200).json(parking)
+})
+
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
 })
