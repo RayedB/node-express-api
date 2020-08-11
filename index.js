f94cd89c7f73a6d79b936ddf8a92d91f1538c8c6
Rayed <rayed.benbrahim@gmail.com>
2020-08-11 16:02:33 +0200

    feat(index):get parkings route

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const parkings = require('./parkings.json')
+
+app.get('/parkings', (req,res) => {
+    res.status(200).json(parkings)
+})
 
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
