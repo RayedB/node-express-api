8580af0599feef7919d889b95724b8c793284a04
Rayed <rayed.benbrahim@gmail.com>
2020-08-12 08:33:46 +0200

    feat(index):post parking/ route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const parkings = require('./parkings.json')
 
+app.use(express.json())
+
 app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
@@ -10,6 +12,11 @@ app.get('/parkings/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const parking = parkings.find(parking => parking.id === id)
     res.status(200).json(parking)
+})
+
+app.post('/parkings', (req,res) => {
+    parkings.push(req.body)
+    res.status(200).json(parkings)
 })
 
 app.listen(8080, () => {
