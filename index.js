5bb5e531f33b54b33385065792298249572cb6db
Rayed <rayed.benbrahim@gmail.com>
2020-08-14 13:45:04 +0200

    feat(index):reservation.json file

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ app.post('/parkings', (req,res) => {
     parkings.push(req.body)
     res.status(200).json(parkings)
 })
+app.put('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parking.name =req.body.name,
+    parking.city =req.body.city,
+    parking.type =req.body.type,
+    res.status(200).json(parking)
+})
+
+app.delete('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parkings.splice(parkings.indexOf(parking),1)
+    res.status(200).json(parkings)
+})
 
 app.listen(8080, () => {
     console.log("Serveur à l'écoute")
