e2f43161ca46432b09e1bc5ee050bfc59a426be4
Rayed <rayed.benbrahim@gmail.com>
2020-08-29 07:47:46 +0200

    feat(index):add mongodb to project

--- index.js
+++ index.js
@@ -2,16 +2,40 @@ const express = require('express')
 const app = express()
 const parkings = require('./parkings.json')
 
+/**
+ * Import MongoClient & connexion à la DB
+ */
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'parkingApi';
+let db
+MongoClient.connect(url, function(err, client) {
+  console.log("Connected successfully to server");
+  db = client.db(dbName);
+});
+
+
 app.use(express.json())
 
-app.get('/parkings', (req,res) => {
-    res.status(200).json(parkings)
+app.get('/parkings', async (req,res) => {
+    try {
+        const docs = await db.collection('parkings').find({}).toArray()
+        res.status(200).json(docs)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
 })
 
-app.get('/parkings/:id', (req,res) => {
+app.get('/parkings/:id', async (req,res) => {
     const id = parseInt(req.params.id)
-    const parking = parkings.find(parking => parking.id === id)
-    res.status(200).json(parking)
+    try {
+        const docs = await db.collection('parkings').find({id}).toArray()
+        res.status(200).json(docs)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
 })
 
 app.post('/parkings', (req,res) => {
