5c3758a31d42bc8e54d016e7506f2893e25dc754
Rayed <rayed.benbrahim@gmail.com>
2020-08-29 08:20:35 +0200

    feat(index):add mongodb to project

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/parkings', async (req,res) => {
 app.get('/parkings/:id', async (req,res) => {
     const id = parseInt(req.params.id)
     try {
-        const docs = await db.collection('parkings').find({id}).toArray()
+        const docs = await db.collection('parkings').findOne({id})
         res.status(200).json(docs)
     } catch (err) {
         console.log(err)
@@ -38,24 +38,50 @@ app.get('/parkings/:id', async (req,res) => {
     }
 })
 
-app.post('/parkings', (req,res) => {
-    parkings.push(req.body)
-    res.status(200).json(parkings)
+app.post('/parkings', async (req,res) => {
+    try {
+        const parkingData = req.body
+        const parking = await db.collection('parkings').insertOne(parkingData)
+        res.status(200).json(parking)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+    
 })
-app.put('/parkings/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parking.name =req.body.name,
-    parking.city =req.body.city,
-    parking.type =req.body.type,
-    res.status(200).json(parking)
+app.put('/parkings/:id', async (req,res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const replacementParking = req.body
+        const parking = await db.collection('parkings').replaceOne({id},replacementParking)
+        res.status(200).json(parking)
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
 })
 
-app.delete('/parkings/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parkings.splice(parkings.indexOf(parking),1)
-    res.status(200).json(parkings)
+app.patch('/parkings/:id', async (req,res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const replacementParking = req.body
+        const parking = await db.collection('parkings').updateOne({id}, {$set: replacementParking}, {upsert:true})
+        res.status(200).json(parking)
+    } catch (err) {
+        console.log(err)
+        throw err
+    } 
+})
+
+app.delete('/parkings/:id', async (req,res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const parking = await db.collection('parkings').deleteOne({id})
+        res.status(200).json(parking)
+    } catch (err) {
+        console.log(err)
+        throw err
+    } 
 })
 
 app.listen(8080, () => {
